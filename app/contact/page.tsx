1a89806ac5a0ca65460f893bd8ab93e268cf10f5
Rayonnant nyonsa <nyonsaakasha@gmail.com>
2026-01-21 14:23:23 +0630

    Exercie avec useRouter

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,5 +1,17 @@
+'use client'
+import { useRouter } from "next/navigation"
+
+
 export default function ContactPage () {
+    const router = useRouter()
+    const handleSendContactForm = () =>{
+        console.log("Formulaire envoyé")
+     router.push("/blog")
+    }
     return (
+        <>
         <h1>Contactez-nous</h1>
+        <button onClick={handleSendContactForm}>Valider le formulaire</button>
+        </> 
     )
 }
